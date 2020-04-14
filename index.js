09d3f69e0f705d5cf3baa3ea81a79cac79fedb6a
clawyq <clintonlaw96@gmail.com>
2020-04-14 10:33:55 +0800

    port update for heroku

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.listen(3000, () => console.log("done"));
+app.listen(PORT, () => console.log("done"));
 
 app.get("/", async (req, res) => {
     const credentials = fs.readFileSync("credentials.json");
