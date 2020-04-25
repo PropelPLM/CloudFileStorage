82efff63a56e07b500f2765a8f4fc564370cdb46
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-04-25 13:08:17 +0800

    state across oauth

---
+++ api/v0/InstanceManager.js
@@ -0,0 +1,28 @@
+'use strict'
+/**
+ * Time of access and the sessionId will be used to demultiplex different iframe sessions
+ * SessionID: allows different users to use the this app at the same time
+ * Time of Access: differentiates iframe sessions spawned by different pages by the same user
+ *  - especially important for revIds, destination folders
+ * connection, salesforceUrl, namespace
+ */
+
+const instanceMap = {}
+
+module.exports = {
+    start: (sessionId) => {
+        const instanceKey = Date.now.toString() + sessionId;
+        instanceMap[instanceKey] = {};
+        return instanceKey;
+    },
+
+    add: (instanceKey, detailKey, detailValue) => {
+        instanceMap[instanceKey][detailKey] = detailValue;
+    },
+
+    get: (instanceKey, ...detailKeys) => {
+        const requestedDetails = {};
+        detailKeys.forEach(key => requestedDetails[key] = instanceMap[instanceKey][key]);
+        return requestedDetails;
+    }
+}
