dd4af51445f5349958308f73af120fee7a589661
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-04-25 17:11:18 +0800

    fullscale implementation of instanceManager

--- api/v0/lib/GoogleDrive.js
+++ api/v0/lib/GoogleDrive.js
@@ -18,7 +18,7 @@ function createAuthUrl(credentials, instanceKey) {
   ({clientId, clientSecret, redirect_uri} = credentials);
 
   const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirect_uri);
-  InstanceManager.add(instanceKey, { oAuth2Client });
+  InstanceManager.add("instanceKey", { oAuth2Client });
   console.log("oAuth2Client", oAuth2Client);
   return oAuth2Client.generateAuthUrl({
     access_type: "offline",
@@ -31,6 +31,7 @@ function createAuthUrl(credentials, instanceKey) {
 async function getTokens(code, instanceKey) {
   let clientId, clientSecret, oAuth2Client;
   ({ clientId, clientSecret, oAuth2Client } = InstanceManager.get(instanceKey, ["clientId", "clientSecret", "oAuth2Client"]));
+  ({ oAuth2Client } = InstanceManager.get("instanceKey", [oAuth2Client]));
   console.log("oAuth2Client", oAuth2Client);
   oAuth2Client.getToken(code, (err, token) => {
     JsForce.sendTokens({...token, clientId, clientSecret}, instanceKey);
