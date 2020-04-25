61be1b7c17d02f0f1945ff3879d2f1e476eee58e
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-04-25 18:05:35 +0800

    fullscale implementation of instanceManager

--- api/v0/lib/GoogleDrive.js
+++ api/v0/lib/GoogleDrive.js
@@ -12,12 +12,12 @@ const redirect_uris = ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"];
 const actions = {
   driveFiles: "https://www.googleapis.com/auth/drive.file"
 }
-var oAuth2Client;
+
 function createAuthUrl(credentials, instanceKey) {
   let clientId, clientSecret, redirect_uri;
   ({clientId, clientSecret, redirect_uri} = credentials);
 
-  oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirect_uri);
+  const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirect_uri);
   InstanceManager.add("instanceKey", { oAuth2Client });
   return oAuth2Client.generateAuthUrl({
     access_type: "offline",
