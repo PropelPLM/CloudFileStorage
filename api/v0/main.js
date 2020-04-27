899fc538c56e31fb012efef5978ac4e8c01d1481
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-04-27 20:16:05 +0800

    fullscale implementation of instanceManager

--- api/v0/main.js
+++ api/v0/main.js
@@ -28,7 +28,7 @@ app.post("/auth", async (req, res) => {
   ({ sessionId, salesforceUrl, clientId, clientSecret } = req.body);
 
   const instanceKey = await InstanceManager.start(sessionId);
-  GoogleDrive.setAttributeOnForm({ targetWindow });
+  GoogleDrive.setAttributeOnForm({ targetWindow: salesforceUrl });
   const instanceDetails = { salesforceUrl, clientId, clientSecret };
   await Promise.all([
     InstanceManager.add(instanceKey, instanceDetails),
