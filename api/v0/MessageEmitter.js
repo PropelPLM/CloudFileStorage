68fa673530f3c409f623b624c982b390d14e8e6a
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-04-28 11:34:57 +0800

    add emitter class

---
+++ api/v0/MessageEmitter.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// Will be used more widely when there are different storages
+const server = require("./main.js");
+const io = require("socket.io")(server);
+
+module.exports = {
+    setKeyedAttribute: (key, attribute, value) => {
+        const keyedAttribute = {}
+        keyedAttribute[`${key}-${attribute}`] = value;
+        io.emit("setAttribute", keyedAttribute)
+    },
+    postMessage: (topic, payload) => {
+        io.emit(topic, payload);
+    }
+}
