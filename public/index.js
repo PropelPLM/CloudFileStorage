ef63e7c36ba1408c398b18b0762092212445e781
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-04-29 12:50:16 +0800

    redo flow

--- public/index.js
+++ public/index.js
@@ -22,8 +22,9 @@ $(() => {
   //SOCKET IO HELPERS
   const socket = io();
   const url = $(location).attr("href");
-  console.log(url.substr(url.lastIndexOf("/") + 1));
-  $("#debug").text(url.substr(url.lastIndexOf("/") + 1));
+  $("#debug").text('HELLO');
+  $("#debug1").text(url);
+  $("#debug2").text(url.substr(url.lastIndexOf("/") + 1));
   socket.emit('start', url.substr(url.lastIndexOf("/") + 1));
 
   socket.on("setAttribute", object => {
