be662570d0eb57dc021bdbb38be0a478d13861ed
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-04-29 15:57:15 +0800

    redo flow

---
+++ api/v0/Logger.js
@@ -0,0 +1,9 @@
+export function logSuccessResponse(response, functionName) {
+    const logEnding = Object.entries(response).length === 0 && response.constructor === Object ? "" : `: ${JSON.stringify(response)}`;
+    console.log(`${functionName} has succeeded with a response${logEnding}.`);
+    return response;
+}
+export function logErrorResponse(error, functionName) {
+    console.log(`${functionName} has failed due to error: ${JSON.stringify(error)}.`);
+    return error;
+}
