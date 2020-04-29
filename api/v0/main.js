f3920315b1435f8234f389920f15a43414eba7a9
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-04-29 12:25:08 +0800

    redo flow

--- api/v0/main.js
+++ api/v0/main.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../../public")));
 
 app.get("/:sessionId", (req, res) => {
-  res.sendFile("../../public/index.html", { root: __dirname });
+  res.sendFile("index.html", {root: path.join(__dirname, "../../public/")});
 });
 
 app.post("/auth", async (req, res) => {
