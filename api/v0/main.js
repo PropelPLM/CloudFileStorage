385106d6de14721c6a172e5b310eb1ecb892abcc
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-05-08 18:40:31 +0800

    formidable refactor

--- api/v0/main.js
+++ api/v0/main.js
@@ -169,28 +169,29 @@ app.post('/upload/:instanceKey', async (req, res) => {
         file
           .on('data', data => {
             progress = progress + data.length
+
             MessageEmitter.postProgress(instanceKey, 'FRONT_END', progress, fileSize);
             GoogleDrive.uploadFile(instanceKey, data);
           })
-          .on('end', async () => {
-            console.log(10);
-            const file = await GoogleDrive.endUpload(instanceKey);
-            console.log(11);
-            const sfObject = await JsForce.create(file.data, instanceKey);
-            console.log(12);
-            const response = {
-              status: parseInt(file.status),
-              data: {
-                ...file.data,
-                sfId: sfObject.id,
-                revisionId: sfObject.revisionId,
-                salesforceUrl,
-                isNew
-              }
-            }
-            console.log(13);
-            res.status(response.status).send(response.data);
-          })
+        })
+      .on('finish', async () => {
+        console.log(10);
+        const file = await GoogleDrive.endUpload(instanceKey);
+        console.log(11);
+        const sfObject = await JsForce.create(file.data, instanceKey);
+        console.log(12);
+        const response = {
+          status: parseInt(file.status),
+          data: {
+            ...file.data,
+            sfId: sfObject.id,
+            revisionId: sfObject.revisionId,
+            salesforceUrl,
+            isNew
+          }
+        }
+        console.log(13);
+        res.status(response.status).send(response.data);
       })
     req.pipe(form);
     console.log(9);
