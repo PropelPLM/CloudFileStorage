e4b7b552916c770eeb5bf69e2d042aa6926e456e
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-05-12 18:30:47 +0800

    organize to routers

---
+++ api/v0/lib/Logger.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  logSuccessResponse: (response, functionName) => {
+    const logEnding =
+      Object.entries(response).length === 0 && response.constructor === Object
+        ? ''
+        : `: ${JSON.stringify(response)}`;
+    console.log(`\x1b[92m${functionName} succeeded \x1b[39m with a response${logEnding}.`);
+    return response;
+  },
+
+  logErrorResponse: (error, functionName) => {
+      console.log(`\x1b[31m${functionName} failed \x1b[39m due to error: ${JSON.stringify(error)}.`);
+      return error;
+  },
+};
