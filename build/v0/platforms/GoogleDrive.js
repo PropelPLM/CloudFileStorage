45c18261fbe7a8b43115b3acebfd59a7f1514212
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-05-16 16:39:51 +0800

    revamp auth flow

---
+++ build/v0/platforms/GoogleDrive.js
@@ -0,0 +1,163 @@
+'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GoogleDrive = void 0;
+var google = require('googleapis').google;
+var PassThrough = require('stream').PassThrough;
+var _a = require('./Logger'), logSuccessResponse = _a.logSuccessResponse, logErrorResponse = _a.logErrorResponse;
+var MessageEmitter = require('./MessageEmitter');
+var InstanceManager = require('./InstanceManager');
+var GoogleDrive = (function () {
+    function GoogleDrive() {
+        this.redirect_uris = ['urn:ietf:wg:oauth:2.0:oob', 'http://localhost'];
+        this.actions = { driveFiles: 'https://www.googleapis.com/auth/drive.file' };
+    }
+    ;
+    GoogleDrive.prototype.createAuthUrl = function (credentials, instanceKey) {
+        var clientId, clientSecret, redirect_uri;
+        (clientId = credentials.clientId, clientSecret = credentials.clientSecret, redirect_uri = credentials.redirect_uri);
+        var oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirect_uri);
+        InstanceManager.add(instanceKey, { oAuth2Client: oAuth2Client });
+        return oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            prompt: 'consent',
+            scope: this.actions.driveFiles,
+            state: Buffer.from(instanceKey).toString('base64')
+        });
+    };
+    GoogleDrive.prototype.getTokens = function (code, instanceKey) {
+        return __awaiter(this, void 0, void 0, function () {
+            var oAuth2Client;
+            return __generator(this, function (_a) {
+                (oAuth2Client = InstanceManager.get(instanceKey, ['oAuth2Client']).oAuth2Client);
+                oAuth2Client.getToken(code)
+                    .then(function (token) {
+                    logSuccessResponse({}, '[GOOGLE_DRIVE.GET_TOKENS]');
+                    return token;
+                })
+                    .catch(function (err) {
+                    logErrorResponse({}, '[GOOGLE_DRIVE.GET_TOKENS]');
+                    return err;
+                });
+                return [2];
+            });
+        });
+    };
+    GoogleDrive.prototype.authorize = function (instanceKey, clientId, clientSecret, tokens) {
+        return __awaiter(this, void 0, void 0, function () {
+            var oAuth2Client;
+            return __generator(this, function (_a) {
+                try {
+                    oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, this.redirect_uris[0]);
+                    oAuth2Client.setCredentials(tokens);
+                    InstanceManager.add(instanceKey, { oAuth2Client: oAuth2Client });
+                    logSuccessResponse({}, '[GOOGLE_DRIVE.AUTHORIZE]');
+                }
+                catch (err) {
+                    logErrorResponse(err, '[GOOGLE_DRIVE.AUTHORIZE]');
+                }
+                return [2];
+            });
+        });
+    };
+    GoogleDrive.prototype.initUpload = function (instanceKey, _a) {
+        var fileName = _a.fileName, mimeType = _a.mimeType, fileSize = _a.fileSize;
+        return __awaiter(this, void 0, void 0, function () {
+            var destinationFolderId, oAuth2Client, drive, uploadStream, fileMetadata, media, file;
+            var _b;
+            return __generator(this, function (_c) {
+                (_b = InstanceManager.get(instanceKey, ['destinationFolderId', 'oAuth2Client']), destinationFolderId = _b.destinationFolderId, oAuth2Client = _b.oAuth2Client);
+                drive = google.drive({ version: 'v3', auth: oAuth2Client });
+                uploadStream = new PassThrough();
+                fileMetadata = {
+                    name: fileName,
+                    driveId: destinationFolderId,
+                    parents: [destinationFolderId]
+                };
+                media = {
+                    mimeType: mimeType,
+                    body: uploadStream
+                };
+                file = drive.files.create({
+                    resource: fileMetadata,
+                    media: media,
+                    supportsAllDrives: true,
+                    fields: 'id, name, webViewLink, mimeType, fileExtension, webContentLink'
+                }, {
+                    onUploadProgress: function (evt) {
+                        var bytesRead = evt.bytesRead;
+                        InstanceManager.update(instanceKey, 'externalBytes', bytesRead);
+                        MessageEmitter.postProgress(instanceKey, 'Google Drive');
+                        if (bytesRead == fileSize) {
+                            uploadStream.emit('end');
+                        }
+                    }
+                });
+                InstanceManager.addRef(instanceKey, 'uploadStream', uploadStream);
+                InstanceManager.addRef(instanceKey, 'file', file);
+                return [2];
+            });
+        });
+    };
+    GoogleDrive.prototype.uploadFile = function (instanceKey, payload) {
+        return __awaiter(this, void 0, void 0, function () {
+            var uploadStream;
+            return __generator(this, function (_a) {
+                (uploadStream = InstanceManager.getRef(instanceKey, 'uploadStream').uploadStream);
+                uploadStream.write(payload);
+                InstanceManager.update(instanceKey, 'uploadStream', uploadStream);
+                return [2];
+            });
+        });
+    };
+    GoogleDrive.prototype.endUpload = function (instanceKey) {
+        return __awaiter(this, void 0, void 0, function () {
+            var file;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        (file = InstanceManager.getRef(instanceKey, 'file').file);
+                        return [4, file];
+                    case 1: return [2, _a.sent()];
+                }
+            });
+        });
+    };
+    return GoogleDrive;
+}());
+exports.GoogleDrive = GoogleDrive;
---
+++ src/v0/utils/JsForce.ts
@@ -0,0 +1,108 @@
+'use strict';
+
+export {};
+const jsConnect = require('jsforce');
+
+const { logSuccessResponse, logErrorResponse } = require('./Logger');
+const InstanceManager = require('./InstanceManager');
+const MessageEmitter = require('./MessageEmitter');
+
+export class JsForce {
+  
+  async connect(sessionId: string, salesforceUrl: string, instanceKey: string) {
+    try {
+      const connection = new jsConnect.Connection({
+        instanceUrl: salesforceUrl,
+        sessionId
+      });
+      await Promise.all([
+        InstanceManager.add(instanceKey, { connection }),
+        this.setupNamespace(instanceKey)
+      ]);
+      logSuccessResponse({}, '[JSFORCE.CONNECT]');
+    } catch (err) {
+      logErrorResponse(err, '[JSFORCE.CONNECT]');
+    }
+  }
+
+  async sendTokens(tokens: Record<string, string>, instanceKey: string) {
+    const newSetting = {
+      Access_Token__c: tokens.access_token,
+      Refresh_Token__c: tokens.refresh_token,
+      Expiry_Date__c: tokens.expiry_date,
+      Client_Id__c: tokens.clientId,
+      Client_Secret__c: tokens.clientSecret
+    };
+
+    let connection: any, orgNamespace: string;
+    ({ connection, orgNamespace } = InstanceManager.get(instanceKey, ['connection', 'orgNamespace']));
+    try {
+      const upsertedTokens = await connection
+        .sobject(`${orgNamespace}__Cloud_Storage__c`)
+        .upsert({ ...(await this.addNamespace(newSetting, instanceKey)) },`${orgNamespace}__Client_Id__c`);
+
+      logSuccessResponse(upsertedTokens, '[JSFORCE.SEND_TOKENS]');
+      MessageEmitter.postTrigger(instanceKey, 'authComplete', {});
+    } catch (err) {
+      logSuccessResponse(err, '[JSFORCE.SEND_TOKENS]');
+    }
+  }
+
+  async create(file: Record<string, string>, instanceKey: string) {
+    try {
+      let connection: any, orgNamespace: string, revisionId: string, isNew: string, name: string,
+          webViewLink: string, id: string, fileExtension: string, webContentLink: string;
+      ({ connection, orgNamespace, revisionId, isNew } = InstanceManager.get(instanceKey, ['connection', 'orgNamespace', 'revisionId', 'isNew']));
+
+      ({ name, webViewLink, id, fileExtension, webContentLink } = file);
+      const newAttachment: Record<string, string> = {
+        External_Attachment_URL__c: webViewLink,
+        File_Extension__c: fileExtension,
+        Google_File_Id__c: id,
+        External_Attachment_Download_URL__c: webContentLink,
+        Content_Location__c: 'E'
+      };
+
+      if (!isNew) {
+        newAttachment['Item_Revision__c'] = revisionId;
+      }
+
+      const sObject = await connection
+        .sobject(`${orgNamespace}__Document__c`)
+        .create({
+          Name: name,
+          ...(await this.addNamespace(newAttachment, instanceKey))
+        });
+      logSuccessResponse({ sObject }, '[JSFORCE.CREATE]');
+      return { ...sObject, revisionId };
+    } catch (err) {
+      logErrorResponse({ err }, '[JSFORCE.CREATE]');
+    }
+  }
+
+  // UTILS
+  async setupNamespace(instanceKey: string) {
+    let connection: any;
+    ({ connection } = InstanceManager.get(instanceKey, ['connection']));
+    const jsForceRecords = await connection.query(
+      'SELECT NamespacePrefix FROM ApexClass WHERE Name = \'CloudStorageService\' LIMIT 1'
+    );
+    const orgNamespace: string = jsForceRecords.records[0].NamespacePrefix;
+    InstanceManager.add(instanceKey, { orgNamespace });
+    logSuccessResponse({ orgNamespace }, '[JSFORCE.SETUP_NAMESPACE]');
+  }
+
+  async addNamespace(customObject: Record<string, string>, instanceKey: string) {
+    let orgNamespace: string;
+    ({ orgNamespace } = InstanceManager.get(instanceKey, ['orgNamespace']));
+    for (const key in customObject) {
+      Object.defineProperty(
+        customObject,
+        `${orgNamespace}__${key}`,
+        Object.getOwnPropertyDescriptor(customObject, key)!
+      );
+      delete customObject[key];
+    }
+    return customObject;
+  }
+}
