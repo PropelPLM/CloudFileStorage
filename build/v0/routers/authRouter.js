bccbdcf326c0851ce402751eb6af961b1b4b2098
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-05-16 16:39:46 +0800

    revamp auth flow

--- build/v0/routers/authRouter.js
+++ build/v0/routers/authRouter.js
@@ -1,4 +1,15 @@
 'use strict';
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -75,15 +86,20 @@ router.post('/:instanceKey', function (req, res) { return __awaiter(void 0, void
     });
 }); });
 router.get('/callback/google', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var instanceKey, code;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
+    var instanceKey, code, token, clientId, clientSecret;
+    var _a;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
             case 0:
                 instanceKey = Buffer.from(req.query.state, 'base64').toString();
                 code = req.query.code;
                 return [4, GoogleDrive.getTokens(code, instanceKey)];
             case 1:
-                _a.sent();
+                token = _b.sent();
+                (_a = InstanceManager.get(instanceKey, ['clientId', 'clientSecret']), clientId = _a.clientId, clientSecret = _a.clientSecret);
+                return [4, JsForce.sendTokens(__assign(__assign({}, token), { clientId: clientId, clientSecret: clientSecret }), instanceKey)];
+            case 2:
+                _b.sent();
                 res.send('<script>window.close()</script>');
                 return [2];
         }
--- src/v0/routers/authRouter.ts
+++ src/v0/routers/authRouter.ts
@@ -11,8 +11,8 @@ const GoogleDrive = require('../lib/GoogleDrive');
 const JsForce = require('../lib/JsForce');
 
 router.post('/:instanceKey', async (req: any, res: any) => {
-  const instanceKey = req.params.instanceKey;
-  let sessionId, salesforceUrl, clientId, clientSecret;
+  const instanceKey: string = req.params.instanceKey;
+  let sessionId: string, salesforceUrl: string, clientId: string, clientSecret: string;
   ({ sessionId, salesforceUrl, clientId, clientSecret } = req.body);
 
   InstanceManager.register(instanceKey);
@@ -23,8 +23,8 @@ router.post('/:instanceKey', async (req: any, res: any) => {
   ]);
 
   if (clientId && clientSecret) {
-    const credentials = { clientId, clientSecret, redirect_uri: `https://${req.hostname}/auth/callback/google` }; //google can be swapped out
-    const url = GoogleDrive.createAuthUrl(credentials, instanceKey);
+    const credentials: Record<string, string> = { clientId, clientSecret, redirect_uri: `https://${req.hostname}/auth/callback/google` }; //google can be swapped out
+    const url: string = GoogleDrive.createAuthUrl(credentials, instanceKey);
 
     MessageEmitter.setAttribute(instanceKey, 'target-window', salesforceUrl);
     logSuccessResponse(instanceKey, '[END_POINT.AUTH_REDIRECT]');
@@ -38,7 +38,12 @@ router.post('/:instanceKey', async (req: any, res: any) => {
 router.get('/callback/google', async (req: any, res: any) => {
   const instanceKey = Buffer.from(req.query.state, 'base64').toString();
   const code = req.query.code;
-  await GoogleDrive.getTokens(code, instanceKey);
+  const token: Record<string, string> = await GoogleDrive.getTokens(code, instanceKey);
+
+  let clientId: string, clientSecret: string;
+  ({ clientId, clientSecret } = InstanceManager.get(instanceKey, ['clientId', 'clientSecret']));
+  await JsForce.sendTokens({ ...token, clientId, clientSecret }, instanceKey);
+  
   res.send('<script>window.close()</script>');
 });
 
