e408195e227ce8a0c045a683327f40d0fcd6221f
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2020-05-21 17:55:41 +0800

    add google file type

---
+++ src/v0/customTypes/GoogleObjects.d.ts
@@ -0,0 +1,11 @@
+interface GoogleFile {
+    data: {
+        id: string
+        name: string
+        webViewLink: string
+        mimeType: string
+        fileExtension: string 
+        webContentLink: string
+    }
+    status: string
+}
