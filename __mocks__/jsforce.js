205d82f697287f6d43595c0af174928b54cbdb11
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2022-02-04 17:22:28 +0800

    add files

--- __mocks__/jsforce.js
+++
@@ -1,6 +0,0 @@
-const jsConnect = jest.genMockFromModule('jsforce');
-import { JsConnection } from './helperObjects/JsConnection';
-const Connection = jest.fn(() => JsConnection);
-jsConnect.Connection = Connection;
-
-export default jsConnect;
--- __mocks__/xlsx-populate.js
+++
@@ -1,8 +0,0 @@
-const xlsxPopulate = jest.genMockFromModule('xlsx-populate');
-xlsxPopulate.fromBlankAsync = jest.fn(async ()=> {
-  return {
-    outputAsync: jest.fn()
-  }
-});
-
-module.exports = xlsxPopulate;
