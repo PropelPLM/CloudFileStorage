5594842df4ea05ee6f9a79d01b8ea1bdb5209ef6
Clinton Law Yong Qiang <clintonlaw96@gmail.com>
2022-05-13 01:01:10 +0800

    clone into v1

---
+++ src/v1/__tests__/utils/JsForce.test.ts
@@ -0,0 +1,144 @@
+// @ts-nocheck
+// TODO: find another way to enforce type safety OR get ts to realise this is a jest file
+import jsConnect from 'jsforce';
+import JsForce from '../../utils/JsForce';
+import { logErrorResponse, logSuccessResponse } from '../../utils/Logger';
+import InstanceManager from '../../utils/InstanceManager';
+import data from '../data/mockData.json';
+import "../../../../jest.extend";
+
+jest
+  .mock('jsforce')
+  .mock('../../utils/Logger')
+  .mock('../../utils/MessageEmitter')
+  .mock('../../utils/InstanceManager');
+
+const instanceMap: Record<string, Partial<IMap>> = data.instanceMap;
+const instanceKey1 = data.instanceKey1;
+const instanceKey2 = data.instanceKey2;
+
+xdescribe ('JsForce test suite', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(async () => {
+    await MessageEmitter.tearDown('Jsforce');
+  })
+
+  describe('connection setup', () => {
+    it('connect establishes connection to SF Apex', async () => {
+      const sessionId = instanceMap[instanceKey1].sessionId;
+      const salesforceUrl = instanceMap[instanceKey1].salesforceUrl;
+      await JsForce.connect(sessionId, salesforceUrl, instanceKey1);
+
+      expect(Object.values(jsConnect.Connection.mock.calls[0][0]))
+        .toEqual(expect.arrayContaining([sessionId, salesforceUrl]));
+      expect(InstanceManager.upsert).toHaveBeenCalledTimes(2);
+      expect(logSuccessResponse).toHaveBeenCalledTimes(2);
+    });
+
+    it('connect sets up namespace', () => {
+      const sessionId = instanceMap[instanceKey1].sessionId;
+      const salesforceUrl = instanceMap[instanceKey1].salesforceUrl;
+      const spy = jest.spyOn(JsForce, 'setupNamespace');
+      JsForce.connect(sessionId, salesforceUrl, instanceKey1);
+
+      expect(JsForce.setupNamespace).toHaveBeenCalledTimes(1);
+      expect(JsForce.setupNamespace).toHaveBeenCalledWith(instanceKey1);
+    });
+  });
+
+  describe('utils', () => {
+    it('setupNamespace does a callout to query SF Apex', async () => {
+      await JsForce.setupNamespace(instanceKey1);
+
+      expect(InstanceManager.upsert).toBeCalledTimes(1);
+      expect(InstanceManager.get).toHaveBeenCalledTimes(1);
+      expect(logSuccessResponse).toHaveBeenCalledTimes(1);
+      expect(InstanceManager.get.mock.results[0].value.connection.query).toBeCalledTimes(1);
+    });
+
+    it('addNamespace prefixes objects with org namespace', async () => {
+      const obj = {};
+      const properties = ['a', '_','*', '/', '\\', '']
+        .forEach(prop => Object.defineProperty(obj, prop, {}));
+      const namespaced = await JsForce.addNamespace(obj, instanceKey1);
+
+      const namespace = instanceMap[instanceKey1].orgNamespace;
+      const namespacedProperties = Object.keys(namespaced);
+      for (let i in properties) expect(namespacedProperties[i]).stringContaining(namespace);
+      expect(InstanceManager.get).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('sendTokens writes correct tokens to SF Apex', async () => {
+    const settings = {
+      access_token: data.accessToken,
+      refresh_token: data.refreshToken,
+      expiry_date: data.expiryDate,
+      clientId: instanceMap[instanceKey1].clientId,
+      clientSecret : instanceMap[instanceKey1].clientSecret
+    }
+    await JsForce.sendTokens(settings, instanceKey1);
+    expect(InstanceManager.get).toHaveBeenCalledTimes(2); //addnamespace does call
+    expect(logSuccessResponse).toBeCalledTimes(1);
+    expect(InstanceManager.get.mock.results[0].value.connection.sobject).toBeCalledTimes(1);
+
+    const namespace = instanceMap[instanceKey1].orgNamespace;
+    expect(InstanceManager.get.mock.results[0].value.connection.sobject).toBeCalledWith(`${namespace}__Cloud_Storage__c`);
+    expect(InstanceManager.get.mock.results[0].value.connection.sobject.mock.results[0].value.upsert.mock.calls[0][0]).toBeType('object');
+    expect(InstanceManager.get.mock.results[0].value.connection.sobject.mock.results[0].value.upsert.mock.calls[0][1]).toBe(`${namespace}__Client_Id__c`);
+  });
+
+  describe('create triggers item creation on SF Apex', () => {
+    it('writes to existing object', async () => {
+      const file = {
+        name: instanceMap[instanceKey1].fileName,
+        webViewLink: instanceMap[instanceKey1].file.webViewLink,
+        id: instanceMap[instanceKey1].file.id,
+        fileExtension: instanceMap[instanceKey1].file.fileExtension,
+        webContentLink: instanceMap[instanceKey1].file.webContentLink
+      };
+      await JsForce.create(file, instanceKey1);
+
+      expect(InstanceManager.get).toHaveBeenCalledTimes(2); //addnamespace does call
+      expect(logSuccessResponse).toBeCalledTimes(1);
+      expect(InstanceManager.get.mock.results[0].value.connection.sobject).toBeCalledTimes(1);
+
+      const namespace = instanceMap[instanceKey1].orgNamespace;
+      expect(InstanceManager.get.mock.results[0].value.connection.sobject).toBeCalledWith(`${namespace}__Document__c`);
+
+      expect(InstanceManager.get.mock.results[0].value.connection.sobject.mock.results[0].value.create.mock.calls[0][0]).toBeType('object');
+      const itemRev = {};
+      itemRev[`${namespace}__Item_Revision__c`] = instanceMap[instanceKey1].revisionId;
+      expect(InstanceManager.get.mock.results[0].value.connection.sobject.mock.results[0].value.create.mock.calls[0][0])
+        .toMatchObject({
+          Name: instanceMap[instanceKey1].fileName,
+          ...itemRev
+        });
+    });
+
+    it('writes to new object', async () => {
+      const file = {
+        name: instanceMap[instanceKey2].fileName,
+        webViewLink: instanceMap[instanceKey2].file.webViewLink,
+        id: instanceMap[instanceKey2].file.id,
+        fileExtension: instanceMap[instanceKey2].file.fileExtension,
+        webContentLink: instanceMap[instanceKey2].file.webContentLink
+      }
+      await JsForce.create(file, instanceKey2);
+
+      const namespace = instanceMap[instanceKey2].orgNamespace;
+      const itemRev = {};
+      itemRev[`${namespace}__Item_Revision__c`] = instanceMap[instanceKey2].revisionId;
+      expect(InstanceManager.get.mock.results[0].value.connection.sobject.mock.results[0].value.create.mock.calls[0][0])
+        .not.toMatchObject({
+          Name: instanceMap[instanceKey2].fileName,
+          ...itemRev
+        });
+    });
+  });
+
+});
---
+++ src/v1/routers/authRouter.ts
@@ -0,0 +1,70 @@
+'use strict';
+
+import express from 'express';
+const router = express.Router();
+import path from 'path';
+
+import { logSuccessResponse, logErrorResponse } from '../utils/Logger';
+import InstanceManager from '../utils/InstanceManager';
+import MessageEmitter from '../utils/MessageEmitter';
+import GoogleDrive from '../platforms/GoogleDrive/GoogleDrive';
+import JsForce from '../utils/JsForce';
+
+router.get('/:instanceKey', async (req, res)=> {
+  await InstanceManager.register(req.params.instanceKey);
+  res.sendFile('index.html', { root: path.join(__dirname, '../../../public/') });
+})
+
+router.post('/:instanceKey/', async (req: any, res: any) => {
+  let instanceKey: string = req.params.instanceKey;
+  let sessionId: string, salesforceUrl: string, clientId: string, clientSecret: string, tenantId: string;
+  ({ sessionId, salesforceUrl, clientId, clientSecret, tenantId } = req.body);
+
+  InstanceManager.register(instanceKey);
+  const instanceDetails = { salesforceUrl, clientId, clientSecret, tenantId };
+
+  try {
+    await Promise.all([
+      InstanceManager.upsert(instanceKey, instanceDetails),
+      JsForce.connect(sessionId, salesforceUrl, instanceKey)
+    ]);
+
+    if (clientId && clientSecret) {
+      const credentials: Record<string, string> = { clientId, clientSecret, redirect_uri: `https://${req.hostname}/auth/callback/google` }; //google can be swapped out
+      const url: string = GoogleDrive.createAuthUrl(credentials, instanceKey);
+
+      MessageEmitter.setAttribute(instanceKey, 'target-window', salesforceUrl);
+      logSuccessResponse(instanceKey, '[END_POINT.AUTH_REDIRECT]');
+      res.status(200).send({ url });
+    } else {
+      throw new Error('Client Id or secret is missing.')
+    }
+  } catch (err) {
+    logErrorResponse( err, '[END_POINT.AUTH_REDIRECT]');
+    res.status(400).send(`Authorization failed, please check your credentials: ${err}`);
+  }
+});
+
+router.get('/callback/google', async (req: any, res: any) => {
+  const instanceKey = Buffer.from(req.query.state, 'base64').toString();
+  const code = req.query.code;
+  try {
+    const token: Record<string, any> = await GoogleDrive.getTokens!(code, instanceKey);
+    let clientId: string, clientSecret: string;
+    ({ clientId, clientSecret } = await InstanceManager.get(instanceKey, [MapKey.clientId, MapKey.clientSecret]));
+
+    if (token.tokens) {
+      await JsForce.sendTokens({ ...token.tokens, clientId, clientSecret }, instanceKey);
+    } else {
+      throw new Error('No tokens found in Google Drive callback.')
+    }
+    MessageEmitter.postTrigger(instanceKey, 'authComplete', {});
+    logSuccessResponse('MessageEmitted', '[CALLBACK_GOOGLE');
+    res.send('<script>window.close()</script>');
+  } catch (err) {
+    res.status(500).send(`Callback from google has failed: ${err}`);
+    logErrorResponse(err, '[CALLBACK_GOOGLE');
+  }
+});
+
+export default router;
