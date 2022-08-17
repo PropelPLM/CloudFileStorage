bb559229dde0a8ecdb86ceb11d75785e2957ed9e
clawyq <32610179+clawyq@users.noreply.github.com>
2022-08-17 17:10:23 +0800

    PIM-280: setup tasks for Digital Assets as managed package (#13)
    * format
    
    * update packages
    
    * npm install aws-sdk cloudfront
    
    * format jsforce
    
    * persist socket io connections across multiple nodes
    
    * make error handling more robust in message emitter
    
    * sset time out to an acceptable integer value

--- src/v1/routers/uploadRouter.ts
+++ src/v1/routers/uploadRouter.ts
@@ -1,146 +1,242 @@
 'use strict';
 
 import { NextFunction, Request, Response, Router } from 'express';
-import { PassThrough, Stream } from "stream";
+import { PassThrough, Stream } from 'stream';
 import { v4 as uuidv4 } from 'uuid';
 
 const Busboy = require('busboy');
 const router = Router();
 
 import InstanceManager from '../utils/InstanceManager';
-import { logSuccessResponse, logErrorResponse, getPlatform } from '../utils/Logger';
-import { ResponseError, responseGenerator } from '../utils/middleware/responseGenerator';
+import {
+    logSuccessResponse,
+    logErrorResponse,
+    getPlatform,
+} from '../utils/Logger';
+import {
+    ResponseError,
+    responseGenerator,
+} from '../utils/middleware/responseGenerator';
 import MessageEmitter from '../utils/MessageEmitter';
 import JsForce from '../utils/JsForce';
-import { CreatedFileDetails, StoragePlatform } from '../platforms/StoragePlatform';
+import {
+    CreatedFileDetails,
+    StoragePlatform,
+} from '../platforms/StoragePlatform';
 
 router.post('/token', async (req: any, res: any, next: NextFunction) => {
-  const instanceKey = uuidv4();
-  try {
-    const instanceDetails = { ...req.body };
-    await InstanceManager.upsert(instanceKey, instanceDetails);
-    logSuccessResponse({instanceKey}, '[END_POINT.TOKEN]');
-    res.locals.result = { instanceKey };
-  } catch (err) {
-    logErrorResponse(err, '[END_POINT.TOKEN]');
-    res.locals.err = new ResponseError(400, `Failed to receive tokens: ${err}.`);
-  } finally {
-    next();
-  }
+    const instanceKey = uuidv4();
+    try {
+        const instanceDetails = { ...req.body };
+        await InstanceManager.upsert(instanceKey, instanceDetails);
+        logSuccessResponse({ instanceKey }, '[END_POINT.TOKEN]');
+        res.locals.result = { instanceKey };
+    } catch (err) {
+        logErrorResponse(err, '[END_POINT.TOKEN]');
+        res.locals.err = new ResponseError(
+            400,
+            `Failed to receive tokens: ${err}.`
+        );
+    } finally {
+        next();
+    }
 });
 
-router.post('/details/:instanceKey', async (req: any, res: any, next: NextFunction) => {
-  try {
-    const instanceKey = req.params.instanceKey;
-    const instanceDetails = { ...req.body };
-    await InstanceManager.upsert(instanceKey, instanceDetails);
-    logSuccessResponse({ instanceKey }, '[END_POINT.UPLOAD_DETAILS]');
-    res.locals.result = { instanceKey };
-  } catch (err) {
-    logErrorResponse(err , '[END_POINT.UPLOAD_DETAILS]');
-    res.locals.err = new ResponseError(400, `Failed to update upload details ${err}.`);
-  } finally {
-    next();
-  }
-});
-
-router.post('/files/:instanceKey', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    res.setTimeout(9999999999)
-    const instanceKey = req.params.instanceKey;
-
-    const form = new Busboy({ headers: req.headers });
-    let salesforceUrl: string, isNew: string, platform: string;
-    const fileDetailsMap = {} as Record<string, FileDetail>;
-    let fileDetails: FileDetail;
-    ({ salesforceUrl, isNew, platform } = await InstanceManager.get(instanceKey, [MapKey.salesforceUrl, MapKey.isNew, MapKey.platform]));
-    const configuredPlatform: StoragePlatform = await getPlatform(platform, instanceKey);
-    const responses: Record<string,any>[] = [];
-    const promises: any[] = [];
-    let fileSizes: Record<string, number> = {};
-
-    form
-      .on('field', (fieldName: string, value: string) => {
-        if (fieldName == 'fileSize') {
-          let fileName: string, fileSize: any;
-          [fileName, fileSize] =  value.split(`__${instanceKey}__`);
-          fileSizes[fileName] = parseInt(fileSize);
-        }
-      })
-      .on('file', async function(_1: any, fileStream: Stream, fileName: string, _2: any, mimeType: string) {
-        const fileSize: number = fileSizes[fileName];
-        promises.push(
-          new Promise(async (resolve, reject) => {
-            const uploadStream = new PassThrough();
-            fileDetails = { fileName, fileSize, frontendBytes: 0, externalBytes: 0, mimeType, uploadStream };
-            const fileDetailKey: string = createFileDetailKey(fileDetails.fileName);
-            fileDetailsMap[fileDetailKey] = fileDetails;
-            fileDetailsMap[fileDetailKey].file = configuredPlatform.initUpload(instanceKey, uploadStream, fileDetailsMap, fileDetailKey);
-            let progress: number = 0;
-            fileStream
-              .on('data', async (data: Record<string, any>) => {
-                progress = progress + data.length;
-                fileDetails.frontendBytes = progress;
-                MessageEmitter.postProgress(instanceKey, fileDetailsMap, fileDetailKey, 'FRONTEND');
-                configuredPlatform.uploadFile(fileDetailsMap, fileDetailKey, data);
-              })
-              .on('error', err => {
-                logErrorResponse(err, '[END_POINT.UPLOAD_INSTANCE_KEY > BUSBOY]');
-                reject(err);
-              })
-              .on('end', async () => {
-                try {
-                  const file: CreatedFileDetails =
-                    await configuredPlatform.endUpload(fileDetailsMap[fileDetailKey]);
-                  const sfObject = await JsForce.create(file, instanceKey);
-                  const response = {
-                    status: file.status,
-                    data: {
-                      ...file,
-                      sfId: sfObject.id,
-                      revisionId: sfObject.revisionId,
-                    }
-                  };
-                  responses.push(response);
-                  logSuccessResponse(response, '[END_UPLOAD]');
-                  resolve(file);
-                } catch (err) {
-                  logSuccessResponse(err, '[END_UPLOAD]');
-                  reject(err)
-                }
-              });
-            })
-          );
-        })
-      .on('finish', async () => {
+router.post(
+    '/details/:instanceKey',
+    async (req: any, res: any, next: NextFunction) => {
         try {
-          await Promise.all(promises);
-          const response = {
-            salesforceUrl,
-            isNew,
-            responses
-          };
-          logSuccessResponse(response, '[END_POINT.UPLOAD_INSTANCE_KEY > UPLOAD]');
-          res.locals.result = response;
-          next();
-        } catch (err: any) {
-          res.locals.err = new ResponseError(500, `Upload failed: ${err}.`);
-          logErrorResponse(err, '[END_POINT.UPLOAD_INSTANCE_KEY > UPLOAD]');
-          next();
+            const instanceKey = req.params.instanceKey;
+            const instanceDetails = { ...req.body };
+            await InstanceManager.upsert(instanceKey, instanceDetails);
+            logSuccessResponse({ instanceKey }, '[END_POINT.UPLOAD_DETAILS]');
+            res.locals.result = { instanceKey };
+        } catch (err) {
+            logErrorResponse(err, '[END_POINT.UPLOAD_DETAILS]');
+            res.locals.err = new ResponseError(
+                400,
+                `Failed to update upload details ${err}.`
+            );
+        } finally {
+            next();
         }
-      });
-    req.pipe(form);
-  } catch (err) {
-    console.log(err);
-    res.locals.err = new ResponseError(500, `Failed to pipe upload form: ${err}.`);
-    logErrorResponse(err, '[END_POINT.UPLOAD_INSTANCE_KEY > PIPING INTO]');
-    next();
-  }
-})
+    }
+);
+
+router.post(
+    '/files/:instanceKey',
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            res.setTimeout(2147483647);
+            const instanceKey = req.params.instanceKey;
+
+            const form = new Busboy({ headers: req.headers });
+            let salesforceUrl: string, isNew: string, platform: string;
+            const fileDetailsMap = {} as Record<string, FileDetail>;
+            let fileDetails: FileDetail;
+            ({ salesforceUrl, isNew, platform } = await InstanceManager.get(
+                instanceKey,
+                [MapKey.salesforceUrl, MapKey.isNew, MapKey.platform]
+            ));
+            const configuredPlatform: StoragePlatform = await getPlatform(
+                platform,
+                instanceKey
+            );
+            const responses: Record<string, any>[] = [];
+            const promises: any[] = [];
+            let fileSizes: Record<string, number> = {};
+
+            form.on('field', (fieldName: string, value: string) => {
+                if (fieldName == 'fileSize') {
+                    let fileName: string, fileSize: any;
+                    [fileName, fileSize] = value.split(`__${instanceKey}__`);
+                    fileSizes[fileName] = parseInt(fileSize);
+                }
+            })
+                .on(
+                    'file',
+                    async function (
+                        _1: any,
+                        fileStream: Stream,
+                        fileName: string,
+                        _2: any,
+                        mimeType: string
+                    ) {
+                        const fileSize: number = fileSizes[fileName];
+                        promises.push(
+                            new Promise(async (resolve, reject) => {
+                                const uploadStream = new PassThrough();
+                                fileDetails = {
+                                    fileName,
+                                    fileSize,
+                                    frontendBytes: 0,
+                                    externalBytes: 0,
+                                    mimeType,
+                                    uploadStream,
+                                };
+                                const fileDetailKey: string =
+                                    createFileDetailKey(fileDetails.fileName);
+                                fileDetailsMap[fileDetailKey] = fileDetails;
+                                fileDetailsMap[fileDetailKey].file =
+                                    configuredPlatform.initUpload(
+                                        instanceKey,
+                                        uploadStream,
+                                        fileDetailsMap,
+                                        fileDetailKey
+                                    );
+                                let progress: number = 0;
+                                fileStream
+                                    .on(
+                                        'data',
+                                        async (data: Record<string, any>) => {
+                                            progress = progress + data.length;
+                                            fileDetails.frontendBytes =
+                                                progress;
+                                            MessageEmitter.postProgress(
+                                                instanceKey,
+                                                fileDetailsMap,
+                                                fileDetailKey,
+                                                'FRONTEND'
+                                            );
+                                            configuredPlatform.uploadFile(
+                                                fileDetailsMap,
+                                                fileDetailKey,
+                                                data
+                                            );
+                                        }
+                                    )
+                                    .on('error', (err) => {
+                                        logErrorResponse(
+                                            err,
+                                            '[END_POINT.UPLOAD_INSTANCE_KEY > BUSBOY]'
+                                        );
+                                        reject(err);
+                                    })
+                                    .on('end', async () => {
+                                        try {
+                                            const file: CreatedFileDetails =
+                                                await configuredPlatform.endUpload(
+                                                    fileDetailsMap[
+                                                        fileDetailKey
+                                                    ]
+                                                );
+                                            const sfObject =
+                                                await JsForce.create(
+                                                    file,
+                                                    instanceKey
+                                                );
+                                            const response = {
+                                                status: file.status,
+                                                data: {
+                                                    ...file,
+                                                    sfId: sfObject.id,
+                                                    revisionId:
+                                                        sfObject.revisionId,
+                                                },
+                                            };
+                                            responses.push(response);
+                                            logSuccessResponse(
+                                                response,
+                                                '[END_UPLOAD]'
+                                            );
+                                            resolve(file);
+                                        } catch (err) {
+                                            logSuccessResponse(
+                                                err,
+                                                '[END_UPLOAD]'
+                                            );
+                                            reject(err);
+                                        }
+                                    });
+                            })
+                        );
+                    }
+                )
+                .on('finish', async () => {
+                    try {
+                        await Promise.all(promises);
+                        const response = {
+                            salesforceUrl,
+                            isNew,
+                            responses,
+                        };
+                        logSuccessResponse(
+                            response,
+                            '[END_POINT.UPLOAD_INSTANCE_KEY > UPLOAD]'
+                        );
+                        res.locals.result = response;
+                        next();
+                    } catch (err: any) {
+                        res.locals.err = new ResponseError(
+                            500,
+                            `Upload failed: ${err}.`
+                        );
+                        logErrorResponse(
+                            err,
+                            '[END_POINT.UPLOAD_INSTANCE_KEY > UPLOAD]'
+                        );
+                        next();
+                    }
+                });
+            req.pipe(form);
+        } catch (err) {
+            console.log(err);
+            res.locals.err = new ResponseError(
+                500,
+                `Failed to pipe upload form: ${err}.`
+            );
+            logErrorResponse(
+                err,
+                '[END_POINT.UPLOAD_INSTANCE_KEY > PIPING INTO]'
+            );
+            next();
+        }
+    }
+);
 
 function createFileDetailKey(fileName: string): string {
-  let fileDetailKey: string = Math.random().toString(); //use uuid nextt time
-  return `${fileName}_${fileDetailKey.substring(2)}`;
+    let fileDetailKey: string = Math.random().toString(); //use uuid nextt time
+    return `${fileName}_${fileDetailKey.substring(2)}`;
 }
 
 router.use(responseGenerator);
--- src/v1/utils/InstanceManager.ts
+++ src/v1/utils/InstanceManager.ts
@@ -5,58 +5,73 @@ import { createClient, RedisClientType } from 'redis';
 let redisClient: RedisClientType;
 
 function removeEmpty(obj: Partial<Record<MapKey, any>>) {
-  return Object.entries(obj)
-    .filter(([_, v]) => v != null)
-    .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {});
+    return Object.entries(obj)
+        .filter(([_, v]) => v != null)
+        .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {});
 }
 
 const NESTED_FILE_DETAILS_FIELD = 'fileDetails';
 
 export default {
-  connectToRedisServer: async () => {
-    const client: RedisClientType = createClient({
-      url: process.env.REDIS_URL,
-    });
-    client.on('error', err => console.error('Redis client error:', err));
-    redisClient = client;
-    await redisClient.connect();
-  },
+    connectToRedisServer: async () => {
+        const client: RedisClientType = createClient({
+            url: process.env.REDIS_URL,
+        });
+        client.on('error', (err) => console.error('Redis client error:', err));
+        redisClient = client;
+        await redisClient.connect();
+    },
 
-  register: (instanceKey: string) => {
-    console.log(`register ${instanceKey}`);
-    // redisClient.hSet(instanceKey, {});
-  },
+    register: (instanceKey: string) => {
+        console.log(`register ${instanceKey}`);
+        // redisClient.hSet(instanceKey, {});
+    },
 
-  checkRegistration: async (instanceKeyOrOrgUrl: string): Promise<boolean> => {
-    return !! await redisClient.exists(instanceKeyOrOrgUrl);
-  },
+    checkRegistration: async (
+        instanceKeyOrOrgUrl: string
+    ): Promise<boolean> => {
+        return !!(await redisClient.exists(instanceKeyOrOrgUrl));
+    },
 
-  upsert: async (instanceKey: string, keyValuePairs: Partial<Record<MapKey, any>>) => {
-    try {
-      await redisClient.hSet(instanceKey, removeEmpty(keyValuePairs));
-    } catch (error) {
-      throw new Error(`Failed to update ${instanceKey} in InstanceManager: ${JSON.stringify(keyValuePairs)}. Error: ${error}`);
-    }
-  },
-
-  get: async (instanceKey: string, detailKeys: MapKey[]): Promise<Partial<Record<string, any>>> => {
-    try {
-      const requestedDetails: Partial<Record<string, any>> = {};
-      const redisResult: Record<string, string> = await redisClient.hGetAll(instanceKey);
-
-      detailKeys.forEach((key: string) => {
-        if (key == NESTED_FILE_DETAILS_FIELD) {
-          redisResult[NESTED_FILE_DETAILS_FIELD] = JSON.parse(redisResult[NESTED_FILE_DETAILS_FIELD])
+    upsert: async (
+        instanceKey: string,
+        keyValuePairs: Partial<Record<MapKey, any>>
+    ) => {
+        try {
+            await redisClient.hSet(instanceKey, removeEmpty(keyValuePairs));
+        } catch (error) {
+            throw new Error(
+                `Failed to update ${instanceKey} in InstanceManager: ${JSON.stringify(
+                    keyValuePairs
+                )}. Error: ${error}`
+            );
         }
-        requestedDetails[key] = redisResult[key];
-      })
+    },
 
-      return requestedDetails;
-    } catch (err) {
-      console.log(err);
-      throw new Error(`InstanceManager does not contain requested keys:
+    get: async (
+        instanceKey: string,
+        detailKeys: MapKey[]
+    ): Promise<Partial<Record<string, any>>> => {
+        try {
+            const requestedDetails: Partial<Record<string, any>> = {};
+            const redisResult: Record<string, string> =
+                await redisClient.hGetAll(instanceKey);
+
+            detailKeys.forEach((key: string) => {
+                if (key == NESTED_FILE_DETAILS_FIELD) {
+                    redisResult[NESTED_FILE_DETAILS_FIELD] = JSON.parse(
+                        redisResult[NESTED_FILE_DETAILS_FIELD]
+                    );
+                }
+                requestedDetails[key] = redisResult[key];
+            });
+
+            return requestedDetails;
+        } catch (err) {
+            console.log(err);
+            throw new Error(`InstanceManager does not contain requested keys:
         ${detailKeys.join(', ')}
       `);
-    }
-  }
+        }
+    },
 };
