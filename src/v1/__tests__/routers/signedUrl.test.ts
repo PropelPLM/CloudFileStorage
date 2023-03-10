9cb345c7ad31e5e5a7fc9e555baec5e68b96c2aa
mikef <mike.fullmore@propelplm.com>
2023-03-10 12:33:54 -0800

    adding tests but not really

---
+++ src/v1/__tests__/routers/signedUrl.test.ts
@@ -0,0 +1,10 @@
+//@ts-nocheck
+'use strict';
+
+import AWS from '../../platforms/AWS/AWS';
+
+jest.mock('AWS');
+
+describe("signed url route test", () => {
+    // todo fill in when I know what I am doing... later today
+});
