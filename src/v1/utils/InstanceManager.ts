996aef0ab25c8976cc12b35706aabb684cc19f2f
prateekshal <pratu.sl@gmail.com>
2023-03-28 17:45:22 -0500

    MVP-23189: testing S3 doc upload

--- src/v1/utils/InstanceManager.ts
+++ src/v1/utils/InstanceManager.ts
@@ -45,6 +45,8 @@ export default {
     ) => {
         try {
             const dbItemsToCommit = removeEmpty(keyValuePairs);
+            console.log('-----------------DB ITEMS');
+            console.log(dbItemsToCommit);
             await redisClient.hSet(instanceKey, dbItemsToCommit);
             logSuccessResponse(dbItemsToCommit, '[DB.UPSERT');
         } catch (error) {
