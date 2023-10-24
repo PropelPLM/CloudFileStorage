7642d79ab9a394d5279b42a35d9011a14811e912
clawyq <clintonlaw96@gmail.com>
2023-10-24 13:18:38 +0800

    update authProvider implementation

--- src/v1/platforms/Office365/AuthProvider.ts
+++ src/v1/platforms/Office365/AuthProvider.ts
@@ -1,41 +1,24 @@
 import { AuthenticationProvider } from '@microsoft/microsoft-graph-client';
-import qs from 'qs';
-import axios, { AxiosRequestConfig } from 'axios';
+import { ClientSecretCredential } from '@azure/identity';
 
 class AuthProvider implements AuthenticationProvider {
 
   private readonly scope: string = 'https://graph.microsoft.com/.default offline_access';
-  private readonly grantType: string = 'client_credentials';
-  private clientId: string;
-  private clientSecret: string;
-  private tenantId: string;
+  private clientCredential: any;
 
   constructor(clientId: string, clientSecret: string, tenantId: string) {
-    this.clientId = clientId;
-    this.clientSecret = clientSecret;
-    this.tenantId = tenantId;
-  }
-
-  private generateTokenEndpoint(tenantId: string): string {
-    if (tenantId == null) throw new Error('TenantId not passed into authenticator.')
-    return `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`;
+    this.clientCredential = new ClientSecretCredential(tenantId, clientId, clientSecret)
   }
 
   public async getAccessToken(): Promise<string> {
-    const postData: Record<string, string> = {
-      client_id: this.clientId,
-      scope: this.scope,
-      client_secret: this.clientSecret,
-      grant_type: this.grantType
-    };
-    const options: AxiosRequestConfig = {
-      method: 'POST',
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      data: qs.stringify(postData),
-      url: this.generateTokenEndpoint(this.tenantId)
-    };
-    const tokenRequestResponse = await axios(options);
-    return tokenRequestResponse.data.access_token;
+    try {
+      const token = await this.clientCredential.getToken(this.scope);
+      console.log({token})
+      return token ? token.token : null;
+    } catch (err: any) {
+      console.log(`Failed to get AccessToken: ${err.message!}`)
+      throw err
+    }
   }
 }
 
