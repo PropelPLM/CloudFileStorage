ed1fb5c1ae868f108708e7561efc41e475f3b356
clawyq <clintonlaw96@gmail.com>
2023-10-25 06:04:29 +0800

    remove token logging

--- src/v1/platforms/Office365/AuthProvider.ts
+++ src/v1/platforms/Office365/AuthProvider.ts
@@ -13,7 +13,6 @@ class AuthProvider implements AuthenticationProvider {
   public async getAccessToken(): Promise<string> {
     try {
       const token = await this.clientCredential.getToken(this.scope);
-      console.log({token})
       return token ? token.token : null;
     } catch (err: any) {
       console.log(`Failed to get AccessToken: ${err.message!}`)
